be4da0d85584fc29407c676d32fee4666542f55b
Sufia Bano <124654106+sufirang@users.noreply.github.com>
2023-04-11 19:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-// // complete this js code
-// function Person(name, age) {
-// 	this.name = name;
-//   this.age = age;
-// }
-
-// Person.prototype.greet = function() {
-//   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-// }
-
-
-// function Employee(name, age, jobTitle) {
-// 	erson.call(this, name, age);
-//   this.jobTitle = jobTitle;
-// }
-
-// Employee.prototype = Object.create(Person.prototype);
-// Employee.prototype.constructor = Employee;
-
-// Employee.prototype.jobGreet = function() {
-// console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-// }
-
-// // Do not change code below this line
-// window.Person = Person;
-// window.Employee = Employee;
-
+// complete this js code
 function Person(name, age) {
   this.name = name;
   this.age = age;
